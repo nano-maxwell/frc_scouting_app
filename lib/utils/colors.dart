a2bcffe919d8733f7f3fa6132ae69f8bb6139033
Maxwell Morgan <itsmaxwellm@gmail.com>
2025-03-09 13:28:11 -0400

    Minor Description Changes + Utils Folder & Colors

---
+++ lib/utils/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+// Add any main colors for the project here.
+const Color primaryPurpleColor = Color(0xFF8652FF);
+const Color purpleShadowColor = Color(0x4D8652FF);
+const Color whiteColor = Colors.white;
+const Color backgroundColor = Color(0xFF0D1114);
+const Color white50Percent = Color(0x80FFFFFF);
